9d2dd108e048216bfb1566b21304f5b946980b8c
Wkyeu <wkhaiyeu@gmail.com>
2026-07-07 00:47:56 +0800

    added notes

--- verify.py
+++ verify.py
@@ -7,9 +7,9 @@ from deepface import DeepFace
 result = DeepFace.verify(
     img1_path="images/elon_1.jpg",
     img2_path="images/elon_2.jpg",
-    model_name="VGG-Face",
+    model_name="VGG-Face", #beginner model - could be Facenet, ArcFace, Dlib, SFace...
     detector_backend="retinaface",
-    enforce_detection=True
+    enforce_detection=True #if True, deepface will throw an error if it cant find a face in the image at all. If false it will proceed anyway.
 )
 
 print(result)

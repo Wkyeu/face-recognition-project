c2be2d3ff64c2dabcd2a57e31a216a2f3c369f14
Wkyeu <wkhaiyeu@gmail.com>
2026-07-07 00:44:06 +0800

    fixed spelling error

--- verify.py
+++ verify.py
@@ -7,7 +7,7 @@ from deepface import DeepFace
 result = DeepFace.verify(
     img1_path="images/elon_1.jpg",
     img2_path="images/elon_2.jpg",
-    model_name="VCG-Face",
+    model_name="VGG-Face",
     detector_backend="retinaface",
     enforce_detection=True
 )

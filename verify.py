d24be764edd86731032ff8b5ca820019386a31fa
Wkyeu <wkhaiyeu@gmail.com>
2026-07-07 00:43:16 +0800

    updated opencv to retinaface

--- verify.py
+++ verify.py
@@ -1,10 +1,14 @@
+import os
+
+os.environ["TF_USE_LEGACY_KERAS"] = "1"
+
 from deepface import DeepFace
 
 result = DeepFace.verify(
     img1_path="images/elon_1.jpg",
     img2_path="images/elon_2.jpg",
-    model_name="VGG-Face",
-    detector_backend="opencv",
+    model_name="VCG-Face",
+    detector_backend="retinaface",
     enforce_detection=True
 )
 

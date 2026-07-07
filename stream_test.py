c9afc7dbb50b70cf8cc2158d9e1f51056f79b472
Wkyeu <wkhaiyeu@gmail.com>
2026-07-07 22:35:01 +0800

    fixed bug

--- stream_test.py
+++ stream_test.py
@@ -8,6 +8,6 @@ DeepFace.stream(
     model_name="VGG-Face",
     detector_backend="opencv",
     time_threshold=5,
-    frame_threshold=5
+    frame_threshold=5,
     enable_face_analysis=True #shows age/gender/emotion overlay too (False to disable)
 )

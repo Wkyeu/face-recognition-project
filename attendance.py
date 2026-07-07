af5b92fa6d62eef88b245c947c57af849d371024
Wkyeu <wkhaiyeu@gmail.com>
2026-07-07 22:41:57 +0800

    check and create csv with headers (name, timestamp) the first time running

--- attendance.py
+++ attendance.py
@@ -1,7 +1,15 @@
 import os
 os.environ["TF_USE_LEGACY_KERAS"] = "1"
 
+import csv
+import cv2
 from deepface import DeepFace
 
 DB_PATH = "database"
 LOG_FILE = "attendance_log.csv"
+
+#Create the CSV with headers if it does not exist yet
+if not os.path.exists(LOG_FILE):
+    with open(LOG_FILE, mode="w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["Name", "Timestamp"])

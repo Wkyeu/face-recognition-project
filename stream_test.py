f53f3d531366facc0eec96dfa84543a2dd72150b
Wkyeu <wkhaiyeu@gmail.com>
2026-07-07 22:16:45 +0800

    added DeepFace.stream() block

--- stream_test.py
+++ stream_test.py
@@ -2,3 +2,11 @@ import os
 os.environ["TF_USE_LEGACY_KERAS"] = "1"
 
 from deepface import DeepFace
+
+DeepFace.stream(
+    db_path="database"
+    model_name="VGG-Face",
+    detector_backend="opencv",
+    time_threshold=5,
+    frame_threshold=5
+)

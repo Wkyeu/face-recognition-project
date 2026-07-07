a1dca7dad54c1a83351ce9c8969b537241cb322f
Wkyeu <wkhaiyeu@gmail.com>
2026-07-07 22:26:53 +0800

    uses DeepFace.analyze() to estimate attributes

--- face_analysis.py
+++ face_analysis.py
@@ -3,3 +3,11 @@ os.environ["TF_USE_LEGACY_KERAS"] = "1"
 
 from deepface import DeepFace
 
+result = DeepFace.analyze(
+    img_path="images/unknown_face.jpg",
+    actions=['age', 'gender', 'emotion'],
+    detector_backend="retinaface",
+    enforce_detection=True
+)
+
+print(result)

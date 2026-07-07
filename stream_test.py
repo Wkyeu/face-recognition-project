076ef55764f793dc368219e261442ebea87ccf46
Wkyeu <wkhaiyeu@gmail.com>
2026-07-07 22:32:24 +0800

    set enable_face_analysis to True in stream_test.py

--- stream_test.py
+++ stream_test.py
@@ -9,4 +9,5 @@ DeepFace.stream(
     detector_backend="opencv",
     time_threshold=5,
     frame_threshold=5
+    enable_face_analysis=True #shows age/gender/emotion overlay too (False to disable)
 )

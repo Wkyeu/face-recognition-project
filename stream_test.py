3453c7619dbf134b35a4b1b62d72a842af1c33f6
Wkyeu <wkhaiyeu@gmail.com>
2026-07-07 22:18:24 +0800

    fixed bug

--- stream_test.py
+++ stream_test.py
@@ -4,7 +4,7 @@ os.environ["TF_USE_LEGACY_KERAS"] = "1"
 from deepface import DeepFace
 
 DeepFace.stream(
-    db_path="database"
+    db_path="database",
     model_name="VGG-Face",
     detector_backend="opencv",
     time_threshold=5,

6022193621c0c39efae369accd05f0d177dff180
Wkyeu <wkhaiyeu@gmail.com>
2026-07-07 22:07:14 +0800

    use deepface for face recognition from database

--- find_face.py
+++ find_face.py
@@ -2,3 +2,13 @@ import os
 os.environ["TF_USE_LEGACY_KERAS"] = "1"
 
 from deepface import DeepFace
+
+results = DeepFace.find(
+    img_path="images/unknown_face.jpg",
+    db_path="database",
+    model_name="VGG-Face",
+    detector_backend="retinaface",
+    enforce_detection=True
+)
+
+print(results)

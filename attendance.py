aa74c00d974c3dfcbe675d1f3bc983bdcac624c5
Wkyeu <wkhaiyeu@gmail.com>
2026-07-08 22:43:30 +0800

    initiated videocapture using cv2

--- attendance.py
+++ attendance.py
@@ -2,7 +2,7 @@ import os
 os.environ["TF_USE_LEGACY_KERAS"] = "1"
 
 import csv
-import cv2
+import cv2 #handle image IO, image preprocessing(cropping,resizing), connect to webcam and capture videos
 from datetime import datetime
 from deepface import DeepFace
 
@@ -31,3 +31,10 @@ def log_attendance(name):
         writer.writerow([name, now.strftime("%Y-%m-%d %H:%M:%S")])
     last_seen[name] = now
     print(f"Logged: {name} at {now.strftime('%H:%M:%S')}")
+
+cap = cv2.VideoCapture(0)
+frame_count = 0
+
+print("Starting attendance system..... press 'q' to quit")
+
+

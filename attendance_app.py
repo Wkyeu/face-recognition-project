bf5f73d3ea55792c6a8ef79f7338ee284a7e8cfe
Wkyeu <wkhaiyeu@gmail.com>
2026-07-08 23:52:01 +0800

    added some helper functions for future implementations

--- attendance_app.py
+++ attendance_app.py
@@ -23,3 +23,16 @@ if not os.path.exists(LOG_FILE):
     with open(LOG_FILE, mode="w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(["Name", "Timestamp"])
+
+#------------------Helper---------------------------------
+
+def load_log_df():
+    return pd.read_csv(LOG_FILE) #read the csv file and return it as a dataframe
+
+def already_logged_recently(name, log_df):
+    matches = log_df[log_df["Name"] == name] #log_df["Name"] == name:checls every row's Name column against the name we are looking for, returns True/False for each row. log_df[...]: using the True/False list to filter the table down to just the matching rows
+    if matches.empty:
+        return False
+    last_time = pd.to_datetime(matches["Timestamp"]).max() #.max() grabs the most recent timestamp if there are multiple past entries of the same name
+    elapsed = (datetime.now() - last_time).total_seconds() / 60
+    return elapsed < COOLDOWN_MINUTES
